6160479db7a4d1227781957dc8d75f9ffa2536c2
Sandra Gao <sgao60@stuy.edu>
2024-09-27 10:47:19 -0500

    MyArrays.java

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,9 +1,14 @@
 public class MyArrays {
 	public static void main(String[]args) {
-		System.out.println("testing arrayToString()");
+		System.out.println("-----------testing arrayToString()-----------");
 		System.out.println("[0, 0, 0, 1]: " + arrayToString(new int[] {0, 0, 0, 1}));
 		System.out.println("[1, 10, 5, 6, 100]: " + arrayToString(new int[] {1, 10, 5, 6, 100}));
 		System.out.println("[]: " + arrayToString(new int[] {}));
+		
+		System.out.println("-----------testing returnCopy()-----------");
+		int[] og = new int[] {10, 9, 8, 7, 6, 5, 4, 3, 3}
+		int[] copy = returnCopy(og)
+		System.out.println(arrayToString(og) + ": " + arrayToString(copy) + " " + og == copy);
 	}
 	
 	public static String arrayToString(int[] nums) {
@@ -17,5 +22,15 @@ public class MyArrays {
 			}
 		}
 		return s + "]";
+	}
+	
+	public static int[] returnCopy(int[] ary) {
+		int[] copy = new int[ary.length];
+		
+		for (int i = 0; i < ary.length; i++) {
+			copy[i] = ary[i];
+		}
+		
+		return copy;
 	}
 }
