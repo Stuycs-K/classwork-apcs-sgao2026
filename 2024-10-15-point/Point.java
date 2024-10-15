1ac1d2bc5f231db8b59f1eecc07a4ec2843aa49f
Sandra Gao <sgao60@stuy.edu>
2024-10-15 10:40:27 -0500

    nonstatic distanceTo

--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -27,7 +27,7 @@ public class Point{
   Write this method FOURTH, go to the main and do that one first.
   */
   public double distanceTo(Point other){
-    return 0.0;
+    return Math.sqrt((Math.pow(other.x - this.x, 2) + Math.pow(other.x - this.y, 2)));
   }
 
   /*
