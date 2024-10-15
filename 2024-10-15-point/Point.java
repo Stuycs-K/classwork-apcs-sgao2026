99950f0b50310cbb836c3894e38b77c119a3a70d
Sandra Gao <sgao60@stuy.edu>
2024-10-15 10:30:06 -0500

    Point other

--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -7,8 +7,8 @@ public class Point{
 
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
-    //COMPLETE THIS SECOND
-    //Then write test cases.
+    this.x = other.x;
+    this.y = other.y;
   }
 
   //Initialize this Point to have the provided values
