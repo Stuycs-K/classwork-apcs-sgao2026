f3d41609c6c0c6bfeaf2de2c9976fdf9e3b2f639
Sandra Gao <sgao60@stuy.edu>
2024-10-15 10:37:18 -0500

    distance() function in Demo.java

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return 0.0;
+    return Math.sqrt((Math.pow((a.getX() - b.getX()), 2) + Math.pow(a.getY() - b.getY(), 2)));
   }
 
   public static void main(String[]args){
@@ -13,8 +13,11 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     Point p4 = new Point(p3);
+    Point p5 = new Point(p1);
     System.out.println( p3);
     System.out.println(p4);
+    System.out.println(p1);
+    System.out.println(p5);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
