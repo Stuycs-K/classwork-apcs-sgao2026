9d617e618a4213da19e47ac148aafbc11ccbd919
Sandra Gao <sgao60@stuy.edu>
2024-10-31 10:44:59 -0500

    ReadFile.java read main.java

---
+++ 10-31-Advent/ReadFile.java
@@ -0,0 +1,25 @@
+//1 you need all three imports.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {
+
+  public static void main(String[] args) {
+    //2 Opening a file requires a try/catch
+    try {
+      File file = new File("Main.java");//1
+      Scanner input = new Scanner(file);
+
+      while(input.hasNextLine()){
+        System.out.println(input.nextLine());
+      }
+      input.close();//releases the file from your program
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return; //you can return from a void function just don't put a value.
+    }
+  }
+}
