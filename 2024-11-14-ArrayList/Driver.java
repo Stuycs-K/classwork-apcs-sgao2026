53fc1552a2bf37a83e6d61c87918a16ffc8f3c0b
Sandra Gao <sgao60@stuy.edu>
2024-11-14 20:08:55 -0500

    mixLists tested

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class Driver {
   public static void main (String[] args) {
-    ArrayList<String> a1 = ArrayListPractice.createRandomArray(0);
-    ArrayList<String> a2 = ArrayListPractice.createRandomArray(1);
+    ArrayList<String> a1 = ArrayListPractice.createRandomArray(10);
+    ArrayList<String> a2 = ArrayListPractice.createRandomArray(19);
     ArrayList<String> a3 = ArrayListPractice.createRandomArray(20);
 
     System.out.println(a1);
@@ -20,8 +20,12 @@ public class Driver {
     ArrayListPractice.replaceEmpty(a3);
     System.out.println(a3);
     System.out.println(ArrayListPractice.makeReversedList(a3));
-
-
-
+	
+	System.out.println(ArrayListPractice.mixLists(a1, a2));
+	System.out.println(ArrayListPractice.mixLists(a2, a1));
+	System.out.println(ArrayListPractice.mixLists(a2, a3));
+	System.out.println(ArrayListPractice.mixLists(a3, a2));
+	System.out.println(ArrayListPractice.mixLists(a1, a3));
+	System.out.println(ArrayListPractice.mixLists(a3, a1));
   }
 }
