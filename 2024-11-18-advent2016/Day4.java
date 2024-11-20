6bcf9a18559e84f57bb4d67a6683df8ca2b40cc1
Sandra Gao <sgao60@stuy.edu>
2024-11-19 21:01:28 -0500

    input split by new line works

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -6,9 +6,25 @@ import java.util.ArrayList;
 
 public class Day4 {
 	public static void main(String[]args) {
-		
+		run();
 	}
 	public static void run() {
-		
+		String[][] lines = initiate();
+	}
+	
+	public static String[][] initiate() {
+		String[][] lines = new String[947][];
+		try {
+			Scanner in = new Scanner(new File("4input.txt"));
+			int i = 0;
+			while (in.hasNextLine()) {
+				String line = in.nextLine();
+				
+			}
+			System.out.println(Arrays.deepToString(lines));
+		} catch (FileNotFoundException e) {
+			System.out.println("oops");
+		}
+		return lines;
 	}
 }
