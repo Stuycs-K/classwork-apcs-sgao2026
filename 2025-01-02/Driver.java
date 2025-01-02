bd0f475101d4120f601588e6cfc9ddcf6a386774
Sandra Gao <sgao60@stuy.edu>
2025-01-02 11:34:32 -0500

    fib function

---
+++ 2025-01-02/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+	public static void main(String[]args) {
+		System.out.println(fib(Integer.parseInt(args[0])));
+	}
+	public static int fib (int n) {
+		if (n < 2) {
+			return n;
+		} else {
+			return fib(n - 1) + fib(n - 2);
+		}
+	}
+}
